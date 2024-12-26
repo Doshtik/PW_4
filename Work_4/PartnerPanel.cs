2997ac7300f5699487bd3c6098a1ca2f1baa3b68
Doshtik <igor.lizyakinvladislavovic@gmail.com>
2024-12-26 14:20:51 +0500

    Исправление неточностей

--- Work_4/PartnerPanel.cs
+++ Work_4/PartnerPanel.cs
@@ -19,35 +19,32 @@ namespace Work_4
         }
         private Panel InitPanel(int id, string typeAndName, string info, int discount)
         {        
-            Label labelInfo = new Label();
-            labelInfo.AutoSize = true;
-            labelInfo.Dock = DockStyle.Left;
-            labelInfo.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            labelInfo.Location = new Point(15, 35);
-            labelInfo.Name = "labelInfo";
-            labelInfo.Size = new Size(136, 63);
-            labelInfo.TabIndex = 3;
-            labelInfo.Text = info;
-
-            Label labelTypeAndName = new Label();
-            labelTypeAndName.AutoSize = true;
-            labelTypeAndName.Dock = DockStyle.Top;
-            labelTypeAndName.Font = new Font("Segoe UI Semibold", 14F, FontStyle.Bold);
-            labelTypeAndName.Location = new Point(15, 10);
-            labelTypeAndName.Name = "labelTypeAndName";
-            labelTypeAndName.Size = new Size(286, 25);
-            labelTypeAndName.TabIndex = 2;
-            labelTypeAndName.Text = typeAndName;
-
             Label labelDiscount = new Label();
             labelDiscount.AutoSize = true;
             labelDiscount.Dock = DockStyle.Right;
             labelDiscount.Font = new Font("Segoe UI", 14F);
-            labelDiscount.Location = new Point(717, 10);
             labelDiscount.Name = "labelDiscount";
             labelDiscount.Size = new Size(48, 25);
             labelDiscount.TabIndex = 1;
             labelDiscount.Text = $"{discount}%";
+            
+            Label labelTypeAndName = new Label();
+            labelTypeAndName.AutoSize = true;
+            labelTypeAndName.Dock = DockStyle.Top;
+            labelTypeAndName.Font = new Font("Segoe UI Semibold", 14F, FontStyle.Bold);
+            labelTypeAndName.Name = "labelTypeAndName";
+            labelTypeAndName.Size = new Size(286, 25);
+            labelTypeAndName.TabIndex = 2;
+            labelTypeAndName.Text = typeAndName;
+            
+            Label labelInfo = new Label();
+            labelInfo.AutoSize = true;
+            labelInfo.Dock = DockStyle.Left;
+            labelInfo.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            labelInfo.Name = "labelInfo";
+            labelInfo.Size = new Size(136, 63);
+            labelInfo.TabIndex = 3;
+            labelInfo.Text = info;
 
             Label labelId = new Label();
             labelId.Name = "labelId";
@@ -58,7 +55,6 @@ namespace Work_4
             panelPartner.BackColor = Color.White;
             panelPartner.Cursor = Cursors.Hand;
             panelPartner.Dock = DockStyle.Top;
-            panelPartner.Location = new Point(10, 10);
             panelPartner.Margin = new Padding(10);
             panelPartner.Name = "panelPartner";
             panelPartner.Padding = new Padding(15, 10, 15, 10);
