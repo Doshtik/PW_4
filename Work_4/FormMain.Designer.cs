3340ecd195359fe1ff9332fc192cb854c0dd036c
Doshtik <igor.lizyakinvladislavovic@gmail.com>
2025-02-18 09:57:57 +0500

    Пока пусть будет так

--- Work_4/FormMain.Designer.cs
+++ Work_4/FormMain.Designer.cs
@@ -30,12 +30,19 @@
         {
             backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             panelSettings = new Panel();
+            bttnDelete = new Button();
+            bttnUpdate = new Button();
+            bttnCreate = new Button();
             panelPartners = new Panel();
+            panelSettings.SuspendLayout();
             SuspendLayout();
             // 
             // panelSettings
             // 
             panelSettings.BorderStyle = BorderStyle.FixedSingle;
+            panelSettings.Controls.Add(bttnDelete);
+            panelSettings.Controls.Add(bttnUpdate);
+            panelSettings.Controls.Add(bttnCreate);
             panelSettings.Dock = DockStyle.Top;
             panelSettings.Location = new Point(0, 0);
             panelSettings.Margin = new Padding(5);
@@ -43,12 +50,45 @@
             panelSettings.Size = new Size(1257, 73);
             panelSettings.TabIndex = 0;
             // 
+            // bttnDelete
+            // 
+            bttnDelete.Location = new Point(346, 11);
+            bttnDelete.Margin = new Padding(10);
+            bttnDelete.Name = "bttnDelete";
+            bttnDelete.Size = new Size(148, 48);
+            bttnDelete.TabIndex = 2;
+            bttnDelete.Text = "Удалить";
+            bttnDelete.UseVisualStyleBackColor = true;
+            // 
+            // bttnUpdate
+            // 
+            bttnUpdate.Location = new Point(178, 11);
+            bttnUpdate.Margin = new Padding(10);
+            bttnUpdate.Name = "bttnUpdate";
+            bttnUpdate.Size = new Size(148, 48);
+            bttnUpdate.TabIndex = 1;
+            bttnUpdate.Text = "Редктировать";
+            bttnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // bttnCreate
+            // 
+            bttnCreate.Location = new Point(10, 11);
+            bttnCreate.Margin = new Padding(10);
+            bttnCreate.Name = "bttnCreate";
+            bttnCreate.Size = new Size(148, 48);
+            bttnCreate.TabIndex = 0;
+            bttnCreate.Text = "Создать";
+            bttnCreate.UseVisualStyleBackColor = true;
+            bttnCreate.Click += BttnCreate_Click;
+            // 
             // panelPartners
             // 
+            panelPartners.AutoScroll = true;
             panelPartners.BorderStyle = BorderStyle.FixedSingle;
             panelPartners.Dock = DockStyle.Fill;
             panelPartners.Location = new Point(0, 73);
             panelPartners.Name = "panelPartners";
+            panelPartners.Padding = new Padding(10);
             panelPartners.Size = new Size(1257, 677);
             panelPartners.TabIndex = 1;
             // 
@@ -64,6 +104,7 @@
             Name = "FormMain";
             ShowIcon = false;
             Text = "Список партнеров";
+            panelSettings.ResumeLayout(false);
             ResumeLayout(false);
         }
 
@@ -71,5 +112,8 @@
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private Panel panelSettings;
         private Panel panelPartners;
+        private Button bttnCreate;
+        private Button bttnUpdate;
+        private Button bttnDelete;
     }
 }
--- Work_4/FormMain.cs
+++ Work_4/FormMain.cs
@@ -6,25 +6,12 @@ namespace Work_4
     public partial class FormMain : Form
     {
         private Models.AppContext _db;
-        private List<PartnerPanel> _partnerPanels;
+        int _y = 0;
+
 
         public FormMain()
         {
             InitializeComponent();
-            _partnerPanels = InitPanels();
-            int i = 0;
-            while (true)
-            {
-                try
-                {
-                    panelPartners.Controls.Add(_partnerPanels[i++]);
-                }
-                catch
-                {
-                    break;
-                }
-            }
-            
         }
         protected override void OnLoad(EventArgs e)
         {
@@ -34,15 +21,69 @@ namespace Work_4
             _db.FromProductsToPartners.Load();
         }
 
-        private List<PartnerPanel> InitPanels()
+        private void BttnCreate_Click(object sender, EventArgs e)
         {
-            List<PartnerPanel> panels = new List<PartnerPanel>();
+            Panel panel = InitPanel(_y, "ООО|Тмыв Денег", "Директор\n88005553535\nРейтиг:10", 1);
+            panelPartners.Controls.Add(panel);
+        }
 
-            for (int i = 0; i < 5; i++)
-            {
-                panels.Add(new PartnerPanel(1, "qwe", "qwe", 12));
-            }
-            return panels;
+        private Panel InitPanel(int id, string typeAndName, string info, int discount)
+        {
+            Label labelDiscount = new Label();
+            labelDiscount.AutoSize = true;
+            labelDiscount.Dock = DockStyle.Right;
+            labelDiscount.Font = new Font("Segoe UI", 14F);
+            labelDiscount.Name = "labelDiscount";
+            labelDiscount.Size = new Size(48, 25);
+            labelDiscount.TabIndex = 1;
+            labelDiscount.Text = $"{discount}%";
+
+            Label labelTypeAndName = new Label();
+            labelTypeAndName.AutoSize = true;
+            labelTypeAndName.Dock = DockStyle.Top;
+            labelTypeAndName.Font = new Font("Segoe UI Semibold", 14F, FontStyle.Bold);
+            labelTypeAndName.Name = "labelTypeAndName";
+            labelTypeAndName.Size = new Size(286, 25);
+            labelTypeAndName.TabIndex = 2;
+            labelTypeAndName.Text = typeAndName;
+
+            Label labelInfo = new Label();
+            labelInfo.AutoSize = true;
+            labelInfo.Dock = DockStyle.Left;
+            labelInfo.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            labelInfo.Name = "labelInfo";
+            labelInfo.Size = new Size(136, 63);
+            labelInfo.TabIndex = 3;
+            labelInfo.Text = info;
+
+            Label labelId = new Label();
+            labelId.Name = "labelId";
+            labelId.Text = id.ToString();
+            labelId.Visible = false;
+
+            Panel partnerItem = new Panel();
+            partnerItem.BackColor = Color.White;
+            partnerItem.Cursor = Cursors.Hand;
+            partnerItem.Margin = new Padding(10);
+            partnerItem.Name = "partnerItem" + id;
+            partnerItem.Padding = new Padding(15, 10, 15, 10);
+            partnerItem.Size = new Size(panelPartners.Width-30, 120);
+            partnerItem.TabIndex = 0;
+            partnerItem.Left = 10;
+            partnerItem.Top = 10 + _y;
+            _y += partnerItem.Height + 10;
+            partnerItem.Controls.Add(labelInfo);
+            partnerItem.Controls.Add(labelTypeAndName);
+            partnerItem.Controls.Add(labelDiscount);
+            partnerItem.Controls.Add(labelId);
+            partnerItem.Click += ItemSelected_Click; 
+
+            return partnerItem;
+        }
+
+        private void ItemSelected_Click(object sender, EventArgs e)
+        { 
+
         }
     }
 }
